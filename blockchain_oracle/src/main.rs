9e77639694bf5a36bfcf138ecd439abf7709b9e9
Lorenzo Zaccagnini <lorenzozcg@gmail.com>
2022-09-29 21:56:59 +0200

    2 way

--- blockchain_oracle/src/main.rs
+++ blockchain_oracle/src/main.rs
@@ -1,3 +1,4 @@
+use web3::contract::{Contract, Options};
 use web3::futures::{future, StreamExt};
 
 extern crate dotenv;
@@ -11,6 +12,7 @@ async fn main() -> web3::contract::Result<()> {
     let web3_goerli = web3::Web3::new(web3::transports::WebSocket::new(&goerli_api_key).await?);
 
     let web3_mumbai = web3::Web3::new(web3::transports::WebSocket::new(&mumbai_api_key).await?);
+    let web3_gananche = web3::Web3::new(web3::transports::Http::new("http://localhost:8545")?);
 
     //todo listen to events withdraw
     let filter_goerli = web3::types::FilterBuilder::default()
@@ -24,7 +26,7 @@ async fn main() -> web3::contract::Result<()> {
                     .parse()
                     .unwrap(),
             ]),
-            None,
+            Some(vec![]),
             None,
             None,
         )
@@ -35,15 +37,72 @@ async fn main() -> web3::contract::Result<()> {
         .subscribe_logs(filter_goerli)
         .await?;
 
-    sub_goerli
-        .for_each(|log| {
-            println!(
-                "Got log transaction hash: {:?}",
-                log.unwrap().transaction_hash
-            );
-            future::ready(())
+    //execute storage_number function when event happens
+
+    let storage_contract = Contract::from_json(
+        web3_gananche.eth(),
+        "0xba114f724115E2f82Ae1CEF88A171995e49918c6"
+            .parse()
+            .unwrap(),
+        include_bytes!("storage.json"),
+    )
+    .unwrap();
+
+    let ganache_accounts = web3_gananche.eth().accounts().await?;
+
+    let account = ganache_accounts[0];
+
+    let sub = sub_goerli
+        .filter_map(|log| async {
+            println!("Got log transaction hash: {:?}", log);
+            if let Ok(log) = log {
+                let tx = storage_contract
+                    .call("store", (42_u32,), account, Options::default())
+                    .await;
+                println!("TxHash: {:?}", tx);
+                let number_stored: u64 = storage_contract
+                    .query("number", (), None, Options::default(), None)
+                    .await
+                    .unwrap();
+                println!("result {:?}", number_stored);
+                Some(())
+            } else {
+                None
+            }
         })
-        .await;
+        .for_each(|_| async {});
+
+    sub.await;
 
     Ok(())
+}
+
+async fn storage_number(
+    web3: web3::Web3<web3::transports::Http>,
+    contract_address: &str,
+) -> web3::contract::Result<u64> {
+    println!("loading contract");
+
+    let storage_contract = Contract::from_json(
+        web3.eth(),
+        contract_address.parse().unwrap(),
+        include_bytes!("storage.json"),
+    )
+    .unwrap();
+
+    let accounts = web3.eth().accounts().await?;
+
+    let tx = storage_contract
+        .call("store", (42_u32,), accounts[0], Options::default())
+        .await?;
+    println!("TxHash: {}", tx);
+
+    let storage_number: u64 = storage_contract
+        .query("number", (), None, Options::default(), None)
+        .await
+        .unwrap();
+
+    println!("storage_number: {}", storage_number);
+
+    Ok(storage_number)
 }
