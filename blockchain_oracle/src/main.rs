055e9d8e6208b67489929d8ba55a0e6b1f06855c
Lorenzo Zaccagnini <lorenzozcg@gmail.com>
2022-11-13 21:19:05 +0100

    Update main.rs

--- blockchain_oracle/src/main.rs
+++ blockchain_oracle/src/main.rs
@@ -1,6 +1,6 @@
 use ethnum::U256;
-use web3::contract::{Contract, Options};
-use web3::futures::{future, StreamExt};
+use web3::contract::Contract;
+use web3::futures::StreamExt;
 
 extern crate dotenv;
 
@@ -9,12 +9,13 @@ async fn main() -> web3::contract::Result<()> {
     dotenv::dotenv().ok();
     let web3_source_chain_ws =
         web3::Web3::new(web3::transports::WebSocket::new("ws://localhost:8545").await?);
-    let web3_gananche = web3::Web3::new(web3::transports::Http::new("http://localhost:8545")?);
+    let web3_gananche = web3::Web3::new(web3::transports::Http::new("http://localhost:7545")?);
+
     let event_signature = "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef";
 
-    let web3_source_chain_contract = Contract::from_json(
+    let web3_destination_chain_contract = Contract::from_json(
         web3_source_chain_ws.eth(),
-        "0xb0091dfad23CA4669ce69DB67D8A1db68e726321"
+        "0x71a2f3AE2ed1aC64028218407d1797e89CDFC119"
             .parse()
             .unwrap(),
         include_bytes!("GBridgeToken.json"),
@@ -22,7 +23,7 @@ async fn main() -> web3::contract::Result<()> {
     .unwrap();
 
     let filter_ganache = web3::types::FilterBuilder::default()
-        .address(vec!["0xb0091dfad23CA4669ce69DB67D8A1db68e726321"
+        .address(vec!["0x71a2f3AE2ed1aC64028218407d1797e89CDFC119"
             .parse()
             .unwrap()])
         .from_block(web3::types::BlockNumber::Latest)
@@ -40,126 +41,22 @@ async fn main() -> web3::contract::Result<()> {
         .await?;
 
     let sub_ganache_logging = sub_ganache.for_each(|log| async move {
-        /*         let amount = format!("{:?}", log.unwrap().topics[2]);
-        let amount_decoded = U256::from_str_radix(&amount[2..], 16).unwrap();
-        println!("Amount burned: {:?}", amount_decoded); */
-        print!("Log");
-        println!(
-            "Data: {}",
-            format!("0x{}", hex::encode(log.unwrap().data.0))
-        );
+        let address = format!("{:?}", log.clone().unwrap().topics[2]);
+
+        match address.as_str() {
+            "0x0000000000000000000000000000000000000000000000000000000000000000" => {
+                println!("Burned");
+                let amount_decoded =
+                    U256::from_str_radix(&hex::encode(log.unwrap().data.0), 16).unwrap();
+                println!("Amount burned: {}", amount_decoded);
+            }
+            _ => {
+                println!("Transferred");
+            }
+        }
     });
 
     sub_ganache_logging.await;
-
-    /*     let filter_goerli = web3::types::FilterBuilder::default()
-        .address(vec!["0xf6f562525D0801C243177b71E74d99e34AaA2a4F"
-            .parse()
-            .unwrap()])
-        .from_block(web3::types::BlockNumber::Latest)
-        .topics(
-            Some(vec![
-                "0x9b1bfa7fa9ee420a16e124f794c35ac9f90472acc99140eb2f6447c714cad8eb"
-                    .parse()
-                    .unwrap(),
-            ]),
-            Some(vec![]),
-            None,
-            None,
-        )
-        .build();
-
-    let filter_goerli_deposit = web3::types::FilterBuilder::default()
-        .address(vec!["0xf6f562525D0801C243177b71E74d99e34AaA2a4F"
-            .parse()
-            .unwrap()])
-        .from_block(web3::types::BlockNumber::Latest)
-        .topics(
-            Some(vec![
-                "0x5548c837ab068cf56a2c2479df0882a4922fd203edb7517321831d95078c5f62"
-                    .parse()
-                    .unwrap(),
-            ]),
-            Some(vec![]),
-            None,
-            None,
-        )
-        .build();
-
-    let sub_goerli = web3_goerli
-        .eth_subscribe()
-        .subscribe_logs(filter_goerli)
-        .await?;
-
-    let sub_goerli_deposit = web3_goerli
-        .eth_subscribe()
-        .subscribe_logs(filter_goerli_deposit)
-        .await?;
-
-    sub_goerli_deposit
-        .for_each(|log| {
-            //convert hexadecimal to decimal
-            let topics2 = log.unwrap().topics[2];
-            //convert topics2 to string
-            let topics2_string = topics2.to_string();
-
-            let (useless, topics2_without_prefix) = topics2_string.split_at(2);
-            print!("Got 0x: {}", useless);
-            print!("Got topics2: {}", topics2_without_prefix);
-
-            //compare topic2
-            let tp2 = "00000000000000000000000000000000000000000000000000071afd498d0000";
-            if topics2_without_prefix == tp2 {
-                println!("same");
-            } else {
-                println!("different");
-            }
-            let decimal = u128::from_str_radix(&topics2_without_prefix, 16).unwrap_or_else(|e| {
-                panic!("Error parsing hex string: {}", e);
-            });
-
-            println!("Got topics2: {:?}", decimal);
-            future::ready(())
-        })
-        .await;
-
-    //execute storage_number function when event happens
-
-    let storage_contract = Contract::from_json(
-        web3_gananche.eth(),
-        "0x9Da604E24B157aa0b581e58b5d3AD5719B86C843"
-            .parse()
-            .unwrap(),
-        include_bytes!("storage.json"),
-    )
-    .unwrap();
-
-    let ganache_accounts = web3_gananche.eth().accounts().await?;
-
-    let account = ganache_accounts[0];
-
-    let sub = sub_goerli
-        .filter_map(|log| async {
-            println!("Got log WITHDRAW transaction hash: {:?}", log);
-
-            if let Ok(log) = log {
-                let tx = storage_contract
-                    .call("store", (27_u32,), account, Options::default())
-                    .await;
-                println!("TxHash: {:?}", tx);
-                let number_stored: u64 = storage_contract
-                    .query("number", (), None, Options::default(), None)
-                    .await
-                    .unwrap();
-                println!("result {:?}", number_stored);
-                Some(())
-            } else {
-                None
-            }
-        })
-        .for_each(|_| async {});
-
-    sub.await; */
 
     Ok(())
 }
